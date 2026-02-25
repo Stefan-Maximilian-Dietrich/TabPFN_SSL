bf375840a3a5b6624877539fa1bb2f39f06e12ca
Stefan Dietrich <stefan.maximilian.dietrich@gmail.com>
2026-02-25 18:14:25 +0100

    mehr informationen sollen angezeigt werden

--- tests/test_tabpfn_fit_slow.py
+++ tests/test_tabpfn_fit_slow.py
@@ -10,6 +10,7 @@ def _has_hf_token() -> bool:
 
 @pytest.mark.skipif(os.getenv("RUN_SLOW", "0") != "1", reason="Set RUN_SLOW=1 to run TabPFN fit test.")
 @pytest.mark.skipif(not _has_hf_token(), reason="HF token missing (TabPFN v2.5 model download is gated).")
+
 def test_tabpfn_minimal_fit_and_predict():
     from tabpfn import TabPFNClassifier
 
