10b1933cdd9566c05017919aec10f5ee06b4b1d2
Stefan Dietrich <stefan.maximilian.dietrich@gmail.com>
2026-02-25 17:56:27 +0100

    mit hf secret token

--- tests/test_tabpfn_fit_slow.py
+++ tests/test_tabpfn_fit_slow.py
@@ -4,7 +4,12 @@ import numpy as np
 import pytest
 
 
+def _has_hf_token() -> bool:
+    return bool(os.getenv("HF_TOKEN") or os.getenv("HUGGINGFACE_HUB_TOKEN"))
+
+
 @pytest.mark.skipif(os.getenv("RUN_SLOW", "0") != "1", reason="Set RUN_SLOW=1 to run TabPFN fit test.")
+@pytest.mark.skipif(not _has_hf_token(), reason="HF token missing (TabPFN v2.5 model download is gated).")
 def test_tabpfn_minimal_fit_and_predict():
     from tabpfn import TabPFNClassifier
 
