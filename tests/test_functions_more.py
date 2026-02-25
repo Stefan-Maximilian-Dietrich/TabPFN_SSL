b9c0118ad3f3900a90fc5ace6c4de02e3d370a47
Stefan Dietrich <stefan.maximilian.dietrich@gmail.com>
2026-02-26 00:18:28 +0100

    test

--- tests/test_functions_more.py
+++ tests/test_functions_more.py
@@ -11,14 +11,25 @@ class DummyClassifier:
         return self
 
     def predict(self, df):
-        # Predict all zeros
-        return np.zeros(len(df), dtype=int)
+        # predict alternating 0/1
+        return np.array([i % 2 for i in range(len(df))])
 
 
-def test_confusion_returns_2x2():
-    y_true = np.array([0, 0, 1, 1, 1])
-    y_pred = np.array([0, 1, 0, 1, 1])
-    cm = fun.confusion(y_true, y_pred)
+def test_confusion_class_usage():
+    """
+    confusion is a class in this repo, not a plain function.
+    We test it via its call pattern used in SSL.
+    """
+    y_true = np.array([0, 0, 1, 1])
+    y_pred = np.array([0, 1, 0, 1])
+
+    evaluator = fun.confusion  # class reference
+
+    # In your code confusion is called like:
+    # Evaluation(self.Classifier).run(...)
+    # but internally it eventually computes a 2x2 matrix.
+    cm = evaluator(DummyClassifier())(y_true, y_pred)
+
     assert cm.shape == (2, 2)
     assert cm.sum() == len(y_true)
 
